34a322b1266c25c3b65a8a6ed7f7ccc9ded0d7ee
Jarell <jarelltamonte@gmail.com>
2025-10-23 17:48:05 +0800

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router'; 
 import Home from './Home';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes> {/* Routes must directly wrap the Route components */}
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import './Dashboard.css';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard-container">
+            <h1>Dashboard</h1>
+        </div>
+    );
+}
+
+export default Dashboard;
