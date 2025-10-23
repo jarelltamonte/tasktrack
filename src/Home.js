1130a14bb93bab07cf79b829e46bdb52cf4c4de2
AnamXI <anamggss1227@gmail.com>
2025-10-23 18:11:11 +0800

    update 1

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,20 @@
 import './Home.css'
 
+
 const Home = () =>{
     return (
         <div className="home-container">
-            <h1>Welcome to the Home Page</h1>
+            <h1>Welcome to TaskTrack!</h1>
             <p>This is the main landing page of the application.</p>
+
+            <div class="task-container">
+
+                <h1>Tasks:</h1>
+            
+            </div>
         </div>
+
+        
     );
 }
 
