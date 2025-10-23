42c5f989a68d2c4421e7af1db2fd7505ac397f5a
Jarell <jarelltamonte@gmail.com>
2025-10-23 18:10:28 +0800

    added navigation.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router'; 
 import Home from './Home';
 import Dashboard from './Dashboard';
+import Navigation from './Navigation';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes> {/* Routes must directly wrap the Route components */}
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
