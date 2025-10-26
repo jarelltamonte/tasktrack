9bf99686f378fad31134f74f3988610385130d9a
Jarell <jarelltamonte@gmail.com>
2025-10-26 11:50:35 +0800

    added hamburger menu

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,28 +1,48 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaHome, FaFeather} from "react-icons/fa"; // Font Awesome Home
+import { FaHome, FaFeather, FaBars, FaTimes } from "react-icons/fa";
 import { MdDashboard } from "react-icons/md";
-
 import "./Navigation.css";
 import Logo from "./Logo.png";
 
 const Navigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="navigation-bar">
-        <p className="logo-name">TaskTrack <FaFeather/></p>
-      {/* <img src={Logo} alt="Logo" className="logo-image" /> */}
-      <div className="nav-item">
-        <Link to="/home" className="links">
-          <FaHome className="icon" />
-          <span>Home</span>
-        </Link>
+    <>
+      {/* Hamburger icon (mobile only) */}
+      <div className="hamburger" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
-      <div className="nav-item">
-        <Link to="/dashboard" className="links">
-          <MdDashboard className="icon" />
-          <span>Dashboard</span>
-        </Link>
+
+      <div className={`navigation-bar ${menuOpen ? "active" : ""}`}>
+        <p className="logo-name">
+          TaskTrack <FaFeather />
+        </p>
+
+        <div className="nav-item">
+          <Link to="/home" className="links" onClick={() => setMenuOpen(false)}>
+            <FaHome className="icon" />
+            <span>Home</span>
+          </Link>
+        </div>
+
+        <div className="nav-item">
+          <Link
+            to="/dashboard"
+            className="links"
+            onClick={() => setMenuOpen(false)}
+          >
+            <MdDashboard className="icon" />
+            <span>Dashboard</span>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
